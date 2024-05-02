a36080594f7a1803dfffea6dfa4989486ac71703
nischroe <nicolas.schroeder@capgemini.com>
2024-05-02 11:27:59 +0200

    reqs and synth data

--- invent_aigarage_use_case_o2/utils/faiss_storage.py
+++ invent_aigarage_use_case_o2/utils/faiss_storage.py
@@ -3,20 +3,6 @@ import faiss
 import numpy as np
 from datetime import datetime
 
-from .retriever import retriever_model_inference
-
-pd.set_option("display.max_rows", 500)
-pd.set_option("display.width", 500)
-pd.set_option("display.max_columns", 500)
-
-
-##############################
-### FAISS - vector db part ###
-##############################
-# TODO - wrap everything into one class
-# TODO - add feature to only return similar documents if there are some (based on a sim. threshold)
-
-
 def index_initializaton(vector_dim: int, partition_nr: int):
     quantizer = faiss.IndexFlatL2(vector_dim)
     index = faiss.IndexIVFFlat(
@@ -36,24 +22,3 @@ def train_index(index, vectors):
 
 def add_index_vectors(index, vectors):
     return index.add(vectors)
-
-
-def vector_search(search_text, top_k, index):
-    search_vector = retriever_model_inference([search_text])
-    _vector = np.array(search_vector)
-
-    print(_vector.shape)
-
-    faiss.normalize_L2(_vector)
-    index.nprobe = 10  # increase the number of nearby cells to search too with `nprobe`
-    k = min(top_k, index.ntotal)
-
-    print(_vector.shape)
-    print(index.d)
-
-    start_time = datetime.now()
-    distances, ann = index.search(_vector, k=k)
-    end_time = datetime.now()
-    process_time = end_time - start_time
-    print(f"time required for clustered-based FAISS search: {process_time}")
-    return distances, ann
