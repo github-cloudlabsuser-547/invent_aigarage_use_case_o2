04ce7cc069530c255defa9decdf5f4efbdeb3443
nischroe <nicolas.schroeder@capgemini.com>
2024-05-03 17:41:16 +0200

    meta type adjust

--- conversational-ai/src/agents/api/routes.py
+++ conversational-ai/src/agents/api/routes.py
@@ -376,8 +376,6 @@ async def chat_completion(message: agents.api.schemas.UserMessage, db: Session =
     )
     log.info(f"Conversation message id {db_message.id} saved to database")
 
-    print(type(meta_json_data))
-
     return {"api_response": api_response, "relevant_history": chat_history, "metadata": meta_json_data, "ner_recommendations": ner_recommendations}
 
 #### NICOLAS ADDED: MOVE APPROPRIATELY
